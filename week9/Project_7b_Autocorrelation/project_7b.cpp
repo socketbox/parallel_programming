860bf1198b20799901224eb84bd99eba0eb728ea
Casey Boettcher <chb@socketbox.com>
2019-06-08 20:17:42 -0700

    Working on OpenCL

--- week9/Project_7b_Autocorrelation/project_7b.cpp
+++ week9/Project_7b_Autocorrelation/project_7b.cpp
@@ -1,7 +1,10 @@
 #include <cstdlib>
 #include <iostream>
 #include <cstdio>
+
 #include <omp.h>
+#include <cl.h>
+#include <cl_platform.h>
 
 #include "simd.p7b.h"
 
@@ -21,12 +24,23 @@ template <typename numeric> void printArray(int sz, numeric *array);
 
 int main(int argc, char *argv[])
 {
+	if(argc != 3)
+	{
+		fprintf(stderr, "This program takes two file arguments: the CL kernel file\
+				and the signal file, in that order. Exiting.\n.");
+		std::exit(EXIT_FAILURE);
+	}
+
   #ifndef _OPENMP
     fprintf(stderr, "No OpenMP support.\n");
-    exit(1);
+		std::exit(EXIT_FAILURE);
   #endif
 
-  std::FILE *fp = std::fopen( argv[1], "r");
+	#ifndef CL_FILE_NAME
+	#define CL_FILE_NAME	"project7b.cl"
+	#endif 
+  
+	std::FILE *fp = std::fopen( argv[2], "r");
   
   int sigSize;
   getDataSize(fp, &sigSize);
@@ -132,9 +146,6 @@ int main(int argc, char *argv[])
   otsSimd.threads = threads;
   otsSimd.dataSize = sigSize;
 
-  //chb:debug
-  sigSize = 10;
-
   double ompSimd_s = omp_get_wtime();
   for( int shift = 0; shift < sigSize; shift++ )
   {
@@ -146,11 +157,143 @@ int main(int argc, char *argv[])
   
   printf("SIMD: %f\n", otsSimd.time);
 
-  //printFloatArray(sigSize, sums);
+  /****************************************************************************
+   * OpenCL
+  ****************************************************************************/
+  /*
+  Timing otsOpenCl;
+  otsOpenCl.threads = threads;
+  otsOpenCl.dataSize = sigSize;
+
+  double ompSimd_s = omp_get_wtime();
+  for( int shift = 0; shift < sigSize; shift++ )
+  {
+     sumsSimd[shift] = SimdMulSum(&sigArray[0], &sigArray[0 + shift], sigSize);
+  }
+	double ompSimd_f = omp_get_wtime();
+	*/
+	FILE *clfp;
+	clfp = fopen( argv[1], "r" );
+	if( fp == NULL )
+	{
+		fprintf( stderr, "Cannot open OpenCL source file '%s'\n", CL_FILE_NAME );
+		return 1;
+	}
+
+	cl_int status;		// returned status from opencl calls
+				// test against CL_SUCCESS
+
+	// get the platform id:
+
+	cl_platform_id platform;
+	status = clGetPlatformIDs( 1, &platform, NULL );
+	if( status != CL_SUCCESS )
+		fprintf( stderr, "clGetPlatformIDs failed (2)\n" );
+	
+	// get the device id:
+
+	cl_device_id device;
+	status = clGetDeviceIDs( platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL );
+	if( status != CL_SUCCESS )
+		fprintf( stderr, "clGetDeviceIDs failed (2)\n" );
+
+
+ 	cl_context context = clCreateContext( NULL, 1, &device, NULL, NULL, &status );
+	if( status != CL_SUCCESS )
+		fprintf( stderr, "clCreateContext failed\n" );
+
+	// 4. create an opencl command queue:
+
+	cl_command_queue cmdQueue = clCreateCommandQueue( context, device, 0, &status );
+	if( status != CL_SUCCESS )
+		fprintf( stderr, "clCreateCommandQueue failed\n" );
+
+	//chb: already done, no?
+	//float *hArray = new float[ 2*Size ];
+	//float *hSums  = new float[ 1*sigSize ];
+	float *sumsOpenCl  = new float[ sigSize ];
+
+	cl_mem dArray = clCreateBuffer( context, CL_MEM_READ_ONLY,  2*sigSize*sizeof(cl_float), NULL, &status );
+	if( status != CL_SUCCESS )
+		fprintf( stderr, "clCreateBuffer failed (1)\n" );
+	cl_mem dSums  = clCreateBuffer( context, CL_MEM_WRITE_ONLY, 1*sigSize*sizeof(cl_float), NULL, &status );
+	if( status != CL_SUCCESS )
+		fprintf( stderr, "clCreateBuffer failed (1)\n" );
+
+  status = clEnqueueWriteBuffer( cmdQueue, dArray, CL_FALSE, 0, sigSize, sigArray, 0, NULL, NULL );
+	if( status != CL_SUCCESS )
+		fprintf( stderr, "clEnqueueWriteBuffer failed (1)\n" );
+
+
+	//////////// Taken stragith from proj5
+	
+	fseek( fp, 0, SEEK_END );
+	size_t fileSize = ftell( fp );
+	fseek( fp, 0, SEEK_SET );
+	char *clProgramText = new char[ fileSize+1 ];		// leave room for '\0'
+	size_t n = fread( clProgramText, 1, fileSize, fp );
+	clProgramText[fileSize] = '\0';
+	fclose( fp );
+	if( n != fileSize )
+		fprintf( stderr, "Expected to read %d bytes read from '%s' -- actually read %d.\n", fileSize, CL_FILE_NAME, n );
+
+	// create the text for the kernel program:
+
+	char *strings[1];
+	strings[0] = clProgramText;
+	cl_program program = clCreateProgramWithSource( context, 1, (const char **)strings, NULL, &status );
+	if( status != CL_SUCCESS )
+		fprintf( stderr, "clCreateProgramWithSource failed\n" );
+	delete [ ] clProgramText;
+
+	// 8. compile and link the kernel code:
+
+	char *options = { "" };
+	status = clBuildProgram( program, 1, &device, options, NULL, NULL );
+	if( status != CL_SUCCESS )
+	{
+		size_t size;
+		clGetProgramBuildInfo( program, device, CL_PROGRAM_BUILD_LOG, 0, NULL, &size );
+		cl_char *log = new cl_char[ size ];
+		clGetProgramBuildInfo( program, device, CL_PROGRAM_BUILD_LOG, size, log, NULL );
+		fprintf( stderr, "clBuildProgram failed:\n%s\n", log );
+		delete [ ] log;
+	}
+
+	// 9. create the kernel object:
+
+	cl_kernel kernel = clCreateKernel( program, "ArrayMult", &status );
+	if( status != CL_SUCCESS )
+		fprintf( stderr, "clCreateKernel failed\n" );
+
+
+	///////////////// end proj5 cp
+	status = clSetKernelArg( kernel, 0, sizeof(cl_mem), &dArray );
+	status = clSetKernelArg( kernel, 1, sizeof(cl_mem), &dSums  );
+	
+
+
+
+
+
+	
+  
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//and make the global and local work sizes:
+
+	size_t globalWorkSize[3] = { Size,         1, 1 };
+	size_t localWorkSize[3]  = { LOCAL_SIZE,   1, 1 };
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+otsOpenCl.time = ompSimd_f - ompSimd_s;
+  
+  printf("OpenCL: %f\n", otsOpenCl.time);
+
   delete[] sumsNp; 
   delete[] sumsOmp1; 
   delete[] sumsOmp16; 
   delete[] sumsSimd; 
+  delete[] sumsOpenCl;
   delete[] sigArray; 
 
   return 0;
